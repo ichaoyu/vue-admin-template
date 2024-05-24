4848baf440a2529b7807874fef42238dc24039e7
chaoyu <chaoyumail@126.com>
2024-05-24 21:59:30 +0800

    feat: 增加element-plus及对应icon库

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { resolve } from 'node:path';
 
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+const root = process.cwd();
+const pathResolve = (pathname: string) => resolve(root, '.', pathname);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  resolve: {
+    alias: {
+      '@/': pathResolve('src') + '/',
+    },
+  },
+});
