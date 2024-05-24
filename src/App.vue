708ff69e0d40708a5fcb79bc32cbe7dd1a10729c
chaoyu <chaoyumail@126.com>
2024-05-24 16:28:55 +0800

    feat: stylelint配置

--- src/App.vue
+++ src/App.vue
@@ -19,11 +19,13 @@ import HelloWorld from './components/HelloWorld.vue'
   height: 6em;
   padding: 1.5em;
   will-change: filter;
-  transition: filter 300ms;
+  transition: filter 300ms
 }
+
 .logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+  filter: drop-shadow(0 0 2em #646cffaa)
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
